e4c647389fcada5ea87001b8b57d13a9c0353037
SXSQ <1710987943@qq.com>
2023-07-31 23:53:38 +0800

    前端修改

---
+++ sx-view/src/pages/InterfaceInfo/components/CreateModal.tsx
@@ -0,0 +1,25 @@
+import {
+  ProColumns,
+  ProTable,
+} from '@ant-design/pro-components';
+import '@umijs/max';
+import {Modal} from 'antd';
+import React from 'react';
+
+export type props = {
+  colums: ProColumns<API.InterfaceInfo>[];
+  onCancel: () => void;
+  onSubmit: (values: API.InterfaceInfo) => Promise<boolean>;
+  visible: boolean;
+};
+const CreateModal: React.FC<props> = (props) => {
+  const {colums, visible, onCancel, onSubmit} = props
+  return (
+    <Modal open={visible} onCancel={() => onCancel?.()}>
+      <ProTable<API.InterfaceInfo> type='form' columns={colums} onSubmit={async (value) => {
+        onSubmit?.(value)
+      }}/>
+    </Modal>
+  );
+};
+export default CreateModal;
---
+++ sx-view/src/pages/InterfaceInfo/components/UpdateModal.tsx
@@ -0,0 +1,43 @@
+import {
+  ProColumns,
+  ProFormInstance,
+  ProTable,
+} from '@ant-design/pro-components';
+import '@umijs/max';
+import {Modal} from 'antd';
+import React, {useEffect, useRef} from 'react';
+
+export type props = {
+  values: API.InterfaceInfo;
+  colums: ProColumns<API.InterfaceInfo>[];
+  onCancel: () => void;
+  onSubmit: (values: API.InterfaceInfo) => Promise<void>;
+  visible: boolean;
+};
+const UpdateModal: React.FC<props> = (props) => {
+  const {values, colums, visible, onCancel, onSubmit} = props
+
+  const formRef = useRef<ProFormInstance>();
+  useEffect(() => {
+    if (formRef) {
+      formRef?.current?.setFieldsValue(values);
+    }
+  }, [values])
+
+
+  return (
+    <Modal open={visible} footer={null} onCancel={() => onCancel?.()}>
+      <ProTable<API.InterfaceInfo>
+        form={{
+          initialValues: values
+        }}
+        formRef={formRef}
+        type='form'
+        columns={colums}
+        onSubmit={async (value) => {
+          onSubmit?.(value)
+        }}/>
+    </Modal>
+  );
+};
+export default UpdateModal;
