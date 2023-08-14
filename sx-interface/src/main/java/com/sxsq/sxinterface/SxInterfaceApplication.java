d19811d038f327922d9da03d4394f29005b0a2c4
SXSQ <1710987943@qq.com>
2023-08-15 00:28:21 +0800

    0815

--- sx-interface/src/main/java/com/sxsq/sxinterface/SxInterfaceApplication.java
+++ sx-interface/src/main/java/com/sxsq/sxinterface/SxInterfaceApplication.java
@@ -1,4 +1,4 @@
-package com.sxsq.sxinterface;
+package com.SXSQ.sxinterface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
